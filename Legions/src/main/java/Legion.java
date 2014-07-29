2643295c9d111147cd889550afc09eae0b1f5539
Torn <tiarlninbox@gmail.com>
2014-07-29 17:13:36 -0500

    Legion Stuff

--- Legions/src/main/java/Legion.java
+++ Legions/src/main/java/Legion.java
@@ -1,26 +1,22 @@
 import lombok.Getter;
 import lombok.Setter;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 
 /**
  * Created by WaterNode on 7/28/2014.
  */
-public class Legion {
+public class Legion implements Listener {
     FileConfiguration config;
 
     @Getter
     String name;
 
-    @Getter @Setter
+    @Getter
+    @Setter
     int level;
 
     @Getter
@@ -30,72 +26,73 @@ public class Legion {
     ArrayList<String> enemies = new ArrayList<String>();
     ArrayList<String> allies = new ArrayList<String>();
 
-    public Legion(String n, ArrayList<LegionMember> ms, FileConfiguration c)  {
+    public Legion(String n, ArrayList<LegionMember> ms, FileConfiguration c) {
         n = name;
         members = ms;
         config = c;
     }
 
     public boolean onCommand(Player player, String[] args) {
-        switch(args[0].toLowerCase()) {
-            case "invite": case "inv":
-                if(getMember(name).getRank() != Rank.NORMAL) {
+        switch (args[0].toLowerCase()) {
+            case "invite":
+            case "inv":
+                if (getMember(name).getRank() != Rank.NORMAL) {
 
                 } else {
                     player.sendMessage(Config.getMessage("needhigherrank"));
                 }
                 break;
             case "general":
-                if(getMember(name).getRank() == Rank.GENERAL) {
+                if (getMember(name).getRank() == Rank.GENERAL) {
 
                 } else {
                     player.sendMessage(Config.getMessage("needgeneral"));
                 }
                 break;
             case "commander":
-                if(getMember(name).getRank() == Rank.GENERAL) {
+                if (getMember(name).getRank() == Rank.GENERAL) {
 
                 } else {
                     player.sendMessage(Config.getMessage("needgeneral"));
                 }
                 break;
             case "claim":
-                if(getMember(name).getRank() != Rank.NORMAL) {
+                if (getMember(name).getRank() != Rank.NORMAL) {
 
                 } else {
                     player.sendMessage(Config.getMessage("needhigherrank"));
                 }
                 break;
             case "unclaim":
-                if(getMember(name).getRank() != Rank.NORMAL) {
+                if (getMember(name).getRank() != Rank.NORMAL) {
 
                 } else {
                     player.sendMessage(Config.getMessage("needhigherrank"));
                 }
                 break;
             case "attack":
-                if(getMember(name).getRank() != Rank.NORMAL) {
+                if (getMember(name).getRank() != Rank.NORMAL) {
 
                 } else {
                     player.sendMessage(Config.getMessage("needhigherrank"));
                 }
                 break;
             case "ally":
-                if(getMember(name).getRank() != Rank.NORMAL) {
+                if (getMember(name).getRank() != Rank.NORMAL) {
 
                 } else {
                     player.sendMessage(Config.getMessage("needhigherrank"));
                 }
                 break;
             case "neutal":
-                if(getMember(name).getRank() != Rank.NORMAL) {
+                if (getMember(name).getRank() != Rank.NORMAL) {
 
                 } else {
                     player.sendMessage(Config.getMessage("needhigherrank"));
                 }
                 break;
             case "enemy":
-                if(getMember(name).getRank() != Rank.NORMAL) {
+                if (getMember(name).getRank() != Rank.NORMAL) {
 
                 } else {
                     player.sendMessage(Config.getMessage("needhigherrank"));
@@ -109,8 +106,8 @@ public class Legion {
     }
 
     public LegionMember getMember(String name) {
-        for(LegionMember lm : members) {
-            if(lm.getName().equalsIgnoreCase(name)) {
+        for (LegionMember lm : members) {
+            if (lm.getName().equalsIgnoreCase(name)) {
                 return lm;
             }
         }
