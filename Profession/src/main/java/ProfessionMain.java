9bef7cfd0f8eefa6f5665201e83e0f3b49957269
Torn <tiarlninbox@gmail.com>
2014-07-29 17:18:02 -0500

    Professions Project

---
+++ Profession/src/main/java/ProfessionMain.java
@@ -0,0 +1,15 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Ervin on 7/29/2014.
+ */
+public class ProfessionMain extends JavaPlugin {
+
+    public void onDisable() {
+
+    }
+
+    public void onEnable() {
+
+    }
+}
